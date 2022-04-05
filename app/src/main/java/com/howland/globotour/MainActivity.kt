dbf1b9b73c4ff2937bb43b6fcb7cc9ca6c8b8626
bhowland <benjhowland@gmail.com>
2022-04-05 16:48:00 -0500

    first commit

---
+++ app/src/main/java/com/howland/globotour/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.howland.globotour
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
