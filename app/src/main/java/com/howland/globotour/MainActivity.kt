4a01d4a986c96da8ed494f770d010225085a5e72
bhowland <benjhowland@gmail.com>
2022-04-08 21:28:50 -0500

    City fragment is running

--- app/src/main/java/com/howland/globotour/MainActivity.kt
+++ app/src/main/java/com/howland/globotour/MainActivity.kt
@@ -2,15 +2,22 @@ package com.howland.globotour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toolbar        : MaterialToolbar
     private lateinit var navController  : NavController
+    private lateinit var navigationView : NavigationView
+    private lateinit var drawerLayout   : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +25,25 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize Views
         toolbar         = findViewById(R.id.activity_main_toolbar)
+        navigationView = findViewById(R.id.nav_view)
+        drawerLayout = findViewById(R.id.drawer_layout)
 
         // Get NavHostFragment and NavController
         val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
         navController   = navHostFrag.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        navigationView.setupWithNavController(navController)
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isOpen){
+            drawerLayout.close()
+        }else {
+            super.onBackPressed()
+        }
     }
 }
